779f73445577d14b6b894390f4ba70231d419a5f
Maggie <mengtian.thu@gmail.com>
2023-08-25 23:36:11 -0700

    initial commit

---
+++ assets/js/rubber.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+//   $(".rubberBand").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
+//       $(this).removeClass("animated")
+//     })
+
+//   $(".rubberBand").hover(function () {
+//     $(this).addClass("animated");
+//   })
+
+// })
+
+// (function () {
+//   $(".rubberBand").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
+//       $(this).removeClass("animated")
+//     })
+
+//   $(".rubberBand").hover(function () {
+//     $(this).addClass("animated");
+//   })
+
+// })();
